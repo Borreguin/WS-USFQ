19ee664dde2b8f1acd8b1511ede561cdad7887c6
jdsanchez1 <jd_sanchez_17@hotmail.com>
2025-04-26 17:25:44 +0200

    ejercicio 1 taller 1

--- Taller1/P1_TSP/TSP.py
+++ Taller1/P1_TSP/TSP.py
@@ -1,3 +1,5 @@
+from ortools.constraint_solver import pywrapcp, routing_enums_pb2
+
 from typing import List
 
 from Taller1.P1_TSP.util import plotear_ruta, generar_ciudades_con_distancias
@@ -8,8 +10,37 @@ class TSP:
         self.ciudades = ciudades
         self.distancias = distancias
 
-    def encontrar_la_ruta_mas_corta(self):
-        pass
+    def encontrar_la_ruta_mas_corta(self)-> List[str]:
+        n = len(self.distancias)
+        gestor = pywrapcp.RoutingIndexManager(n, 1, 0)
+        modelo = pywrapcp.RoutingModel(gestor)
+        
+        def distancia_callback(i, j):
+            return int(self.distancias[gestor.IndexToNode(i)][gestor.IndexToNode(j)])
+
+        transit_callback_index = modelo.RegisterTransitCallback(distancia_callback)
+        modelo.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+        parametros = pywrapcp.DefaultRoutingSearchParameters()
+        parametros.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC
+        )
+
+        solucion = modelo.SolveWithParameters(parametros)
+
+        if not solucion:
+            print("No se encontró solución")
+            return []
+        indice = modelo.Start(0)
+        ruta_indices = []
+        while not modelo.IsEnd(indice):
+            nodo = gestor.IndexToNode(indice)
+            ruta_indices.append(nodo)
+            indice = solucion.Value(modelo.NextVar(indice))
+        ruta_indices.append(ruta_indices[0])
+
+
+        #pass
         # implementación aquí
 
     def plotear_resultado(self, ruta: List[str], mostrar_anotaciones: bool = True):
