41907bd5087cf8f71c96aaa996a03396f175b314
Daniel <daniel-arias11@hotmail.com>
2025-05-13 18:48:47 -0500

    update project resources

--- ProyectoFinal/VRP/test.py
+++ ProyectoFinal/VRP/test.py
@@ -1,4 +1,43 @@
-def encontrar_la_ruta_mas_corta(self, mipgap, time_limit, tee):
+import subprocess
+import re
+from util import *
+from util_nearest_neighbor import nearest_neighbor
+import sys
+import os
+import pyomo.environ as pyo
+
+class VRP:
+    def __init__(self, ciudades, distancias, heuristics: List[str]):
+        self.max_possible_distance = None
+        self.min_possible_distance = None
+        self.ciudades = ciudades
+        self.distancias = distancias
+        self.heuristics = heuristics
+        self.min_distance = get_min_distance(distancias)
+        self.max_distance = get_max_distance(distancias)
+        self.average_distance = get_average_distance(distancias)
+        self.average_distance_for_city = get_average_distance_for_city(
+            distancias)
+        self.min_distance_for_city = get_min_distances(distancias)
+        self.max_distance_for_city = get_max_distances(distancias)
+        self.cal_min_max_distances()
+
+    def cal_min_max_distances(self):
+        medium_low_distance = (self.min_distance + self.average_distance) / 2
+        self.min_possible_distance = medium_low_distance * \
+            len(self.ciudades) * 0.25
+        self.max_possible_distance = medium_low_distance * \
+            len(self.ciudades) * 0.6
+
+    def print_min_max_distances(self):
+        print(f"Distancia mínima entre nodos: {self.min_distance}")
+        print(f"Distancia máxima entre nodos: {self.max_distance}")
+        print(f"Distancia promedio entre nodos: {self.average_distance}")
+        print(f"Distancia Total mínima posible: {self.min_possible_distance}")
+        print(f"Distancia Total máxima posible: {self.max_possible_distance}")
+        print(f"Heurísticas aplicadas: {self.heuristics}")
+    
+    def encontrar_la_ruta_mas_corta(self, mipgap, time_limit, tee):
         start_time = dt.datetime.now()
 
         _model = pyo.ConcreteModel()
@@ -52,7 +91,20 @@ def encontrar_la_ruta_mas_corta(self, mipgap, time_limit, tee):
             return pyo.Constraint.Skip
         _model.no_self_travel = pyo.Constraint(_model.N, _model.M, rule=rule_asegurar_viaje)
 
-        # Usar solo la de veicnos cercanos
+        # Heurísticas
+        if "vecino_cercano" in self.heuristics:
+            def rule_vecino_cercano(model, i, j):
+                if i == j:
+                    return pyo.Constraint.Skip
+                print(i, self.min_distance, self.average_distance,
+                      self.min_distance_for_city[i], self.max_distance_for_city[i], self.average_distance_for_city[i])
+                if self.average_distance_for_city[i] > self.average_distance:
+                    return pyo.Constraint.Skip
+                expr = model.x[i, j] * self.distancias[i, j] <= (
+                    self.average_distance_for_city[i] + self.min_distance_for_city[i])/2
+                return expr
+            _model.nearest_neighbor = pyo.Constraint(
+                _model.N, _model.M, rule=rule_vecino_cercano)
 
         # Initialize empty set for dynamic constraints (optional)
         # _model.subtour_constraint = pyo.ConstraintList()
